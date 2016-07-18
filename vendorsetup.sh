346c0b9d08d0b7c3ebd41d4851d4eb2e3c9420a5
Kazakov Stepan <kazakov1999stepan1948.12@gmail.com>
2016-07-18 19:22:24 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 # Patches
-if [ ! -e device/huawei/ok ]
+if [ ! -e device/huawei/msm7x27a-common/patches/ok ]
 then
-sh device/huawei/msm7x27a-common/patches/apply.sh
-touch device/huawei/ok
+if [ "$APPPLY_7X27A_PATCHES" != "false" ];then sh device/huawei/msm7x27a-common/patches/apply.sh;fi
+touch device/huawei/msm7x27a-common/patches/ok
 fi
 export KBUILD_BUILD_VERSION=1
 export JAVA_HOME=$(dirname $(dirname $(dirname $(readlink -f $(which java)))))
